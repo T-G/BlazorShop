7b41f4b840edfdb1855d58b0a4c0f0b2d93b7e4c
Theo <pronoygomes@yahoo.com>
2019-11-20 12:24:05 +0000

    =initial commit

---
+++ BlazorShop/Pages/Learn Blazor/LearnBlazor.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorShop.Pages.Learn_Blazor
+{
+    public class LearnBlazorBase : ComponentBase
+    {
+        protected string name = "Spark";
+        protected string WelcomeText = "Time to Learn Balzor";
+
+        public void getName()
+        {
+            name = "Blazor Learner";
+        }
+
+    }
+}
